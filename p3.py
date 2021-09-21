5fff4dd02bfb4f962dea27eb647c5b69045de7d2
Samoei <samoeiayuka@gmail.com>
2021-09-21 20:33:01 +0100

    Finally works

--- p3.py
+++ p3.py
@@ -9,7 +9,7 @@ import time
 # some global variables that need to change as we run the program
 end_of_game = False
 guess = 0
-n0_guesses = 0
+numGuesses = 0
 value = 0
 
 
@@ -199,9 +199,9 @@ def btn_guess_pressed(channel):
     beg = time.time()
     while GPIO.input(btn_submit) == GPIO.LOW:
         time.sleep(0.05)
-    duration = time.time() - start
+    timeHeld = time.time() - beg # how long the button was held for
     # checkif the button is pressed andheld
-    if length > 5:
+    if timeHeld > 2:
         
         GPIO.cleanup() # clear GPIO
         pwmLED.stop() # resetting vals
@@ -222,7 +222,7 @@ def btn_guess_pressed(channel):
         accuracy_leds()
 
     # if it's close enough, adjust the buzzer
-        if (abs(guess-value)<4):
+        if (abs(guess-value)<3):
             trigger_buzzer()
     # if it's an exact guess:
     elif guess == value:
@@ -231,7 +231,7 @@ def btn_guess_pressed(channel):
         GPIO.output(LED_value[0], GPIO.LOW)
         GPIO.output(LED_value[1], GPIO.LOW)
         GPIO.output(LED_value[2], GPIO.LOW)
-        pwm_LED.stop()
+        pwmLED.stop()
         pwm_B.stop()
 
     # - tell the user and prompt them for a name
@@ -259,35 +259,33 @@ def accuracy_leds():
     # - The % brightness should be directly proportional to the % "closeness"
     # - For example if the answer is 6 and a user guesses 4, the brightness should be at 4/6*100 = 66%
     global guess
-    global pwm_LED
+    global pwmLED
     global value
-    pwm_LED.start(50)
+    pwmLED.start(50)
     if (guess < value):
-        pwm_LED.ChangeDutyCycle(int(round((guess/value)*100)))
+        pwmLED.ChangeDutyCycle(int(round((guess/value)*100)))
     else: 
        # if above guess value
-        pwm_LED.ChangeDutyCycle(int(round(((8-guess)/(8-value))*100)))
+        pwmLED.ChangeDutyCycle(int(round(((8-guess)/(8-value))*100)))
 
 pass
 
 # Sound Buzzer
 def trigger_buzzer():
-    dc = 5.0
-    freq = 1.0
     # The buzzer operates differently from the LED
     # While we want the brightness of the LED to change(duty cycle), we want the frequency of the buzzer to change
     # The buzzer duty cycle should be left at 50%
     # If the user is off by an absolute value of 3, the buzzer should sound once every second
-    if (abs(guess-value)==3):
-        freq = 1.0
+    if (abs(guess-value)>=3):
+        pwm_B.ChangeFrequency(1)
     # If the user is off by an absolute value of 2, the buzzer should sound twice every second
     elif (abs(guess-value)==2):
-        freq = 2.0
+        pwm_B.ChangeFrequency(2)
     # If the user is off by an absolute value of 1, the buzzer should sound 4 times a second
     elif (abs(guess-value)==1):
-        freq = 4.0
-    pwm_BUZZER.start(dc)
-    pwm_BUZZER.ChangeFrequency(freq)
+        pwm_B.ChangeFrequency(4)
+    GPIO.output(buzzer, GPIO.HIGH)
+  
     pass
 
 
@@ -302,4 +300,6 @@ if __name__ == "__main__":
     except Exception as e:
         print(e)
     finally:
+        pwmLED.stop()
+        pwm_B.stop()
         GPIO.cleanup()
