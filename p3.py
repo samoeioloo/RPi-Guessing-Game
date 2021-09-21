50cde703e07a9399bf27fde67d58254053a4208b
Samoei <samoeiayuka@gmail.com>
2021-09-21 19:53:17 +0100

    Accuracy_LED method fix

--- p3.py
+++ p3.py
@@ -261,13 +261,16 @@ def accuracy_leds():
 
     # Set the brightness of the LED based on how close the guess is to the answer
 
-    global guess, pwm_LED, value
-    pwm_LED.start(50)
     # - The % brightness should be directly proportional to the % "closeness"
     # - For example if the answer is 6 and a user guesses 4, the brightness should be at 4/6*100 = 66%
-    if (guess <= value):
+    global guess
+    global pwm_LED
+    global value
+    pwm_LED.start(50)
+    if (guess < value):
         pwm_LED.ChangeDutyCycle(int(round((guess/value)*100)))
-    else:
+    else: 
+       # if above guess value
         pwm_LED.ChangeDutyCycle(int(round(((8-guess)/(8-value))*100)))
 
 pass
